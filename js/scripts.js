551e5cbb5eeafe39c9ff8bb17b99b8a80033f3d4
niehusst <liamniehusstaab@gmail.com>
2019-08-25 22:31:46 +0200

    fixed js device width detection for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,29 @@
  * Function to handle bootstrap card element reordering when screen width gets too small
  */
 function orderCardContents() {
-  var mobile_width = 768;
-  var device_width = window.screen.width;
+  var mobile_width = 40;
+  // hacky way of using CSS media queries to find device width
+  var el = document.getElementsByClassName("name");
+  console.log(el.length);
+  var device_width = parseInt(window.getComputedStyle(el[0], null).getPropertyValue('font-size'));
+  console.log(device_width);
+  
   if(device_width <= mobile_width) {
     var elements = document.getElementsByClassName("row-col-changeable");
-    console.log(elements.length);
     var i;
     for(i = 0; i < elements.length; i++) {
       var element = elements[i];
-      console.log(typeof(element));
-
-      if (width_percent <= min_percent) {
-        element.classList.remove("flex-row");
-        element.classList.add("flex-column");
-      } else {
-        element.classList.add("flex-row");
-        element.classList.remove("flex-column");
-      }
+      element.classList.remove("flex-row");
+      element.classList.add("flex-column");
     }
   } else {
     // get window dims / device dims percentage
     var width_percent = window.outerWidth / device_width;
     var min_percent = 0.7254758418740849;
     var elements = document.getElementsByClassName("row-col-changeable");
-    console.log(elements.length);
     var i;
     for(i = 0; i < elements.length; i++) {
       var element = elements[i];
-      console.log(typeof(element));
-
       if (width_percent <= min_percent) {
         element.classList.remove("flex-row");
         element.classList.add("flex-column");

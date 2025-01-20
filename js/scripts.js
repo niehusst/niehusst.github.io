6938372050a5ec4d880ad16a11cc139aa099a1c1
niehusst <liamniehusstaab@gmail.com>
2025-01-20 15:24:17 -0800

    fix: mobile UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
 /**
  * Function to handle bootstrap card element reordering when screen width gets too small
- */
+ * // dont need this anymore becuase I made the CSS suck less
 function orderCardContents() {
-  var mobile_width = 40;
+  let mobile_width = 40;
   // hacky way of using CSS media queries to find device width
-  var el = document.getElementsByClassName("name");
-  console.log(el.length);
-  var device_width = parseInt(window.getComputedStyle(el[0], null).getPropertyValue('font-size'));
-  console.log(device_width);
+  let el = document.getElementsByClassName("name");
+  let device_width = parseInt(window.getComputedStyle(el[0], null).getPropertyValue('font-size'));
   
   if(device_width <= mobile_width) {
-    var elements = document.getElementsByClassName("row-col-changeable");
-    var i;
+    let elements = document.getElementsByClassName("row-col-changeable");
+    let i;
     for(i = 0; i < elements.length; i++) {
-      var element = elements[i];
+      let element = elements[i];
       element.classList.remove("flex-row");
       element.classList.add("flex-column");
     }
   } else {
     // get window dims / device dims percentage
-    var width_percent = window.outerWidth / device_width;
-    var min_percent = 0.7254758418740849;
-    var elements = document.getElementsByClassName("row-col-changeable");
-    var i;
+    let width_percent = window.outerWidth / device_width;
+    let min_percent = 0.7254758418740849;
+    let elements = document.getElementsByClassName("row-col-changeable");
+    let i;
     for(i = 0; i < elements.length; i++) {
-      var element = elements[i];
+      let element = elements[i];
       if (width_percent <= min_percent) {
         element.classList.remove("flex-row");
         element.classList.add("flex-column");
@@ -37,3 +35,4 @@ function orderCardContents() {
 }
 window.addEventListener("resize", orderCardContents);
 $(document).ready(orderCardContents);
+ */
